dfe922c7ede11a80f49ec34b66c361a29b9ff0bd
heifade <heifade@126.com>
2017-12-21 21:12:14 +0800

    init

--- test/file.test.ts
+++ test/file.test.ts
@@ -8,15 +8,16 @@ describe("fs", function() {
 
   it("getAllFiles", async () => {
     let files = await getAllFiles("./test/path");
-    expect(files.length).to.be.equal(3);
+    expect(files.length).to.be.equal(5);
     expect(files[0]).to.be.equal('./test/path/.f2');
     expect(files[1]).to.be.equal('./test/path/f1.txt');
     expect(files[2]).to.be.equal('./test/path/path1/f3.txt');
+    expect(files[3]).to.be.equal('./test/path/path1/path2/f4.txt');
+    expect(files[4]).to.be.equal('./test/path/path12/f5.txt');
   });
 
   it("getAllDirs", async () => {
     let dirs = await getAllDirs("./test/path");
-    console.log(1, dirs);
     expect(dirs.length).to.be.equal(3);
     expect(dirs[0]).to.be.equal('./test/path/path1');
     expect(dirs[1]).to.be.equal('./test/path/path1/path2');
